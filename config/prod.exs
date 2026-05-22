3dd6379cfba13bd486a3452d1402caadad07d7c8
Neo <lovchikovmmark@gmail.com>
2026-05-22 18:13:45 +0300

    edit dockerfile

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,7 @@ config :logger, level: :info
 
 config :chat, Chat.Repo,
   username: System.get_env("DB_USER"),
-  passwword: System.get_env("DB_PASSWORD"),
+  password: System.get_env("DB_PASSWORD"),
   database: System.get_env("DB_NAME"),
   hostname: System.get_env("DB_HOST"),
   port: System.get_env("DB_PORT"),
